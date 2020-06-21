66e7c1d6dc46033d8555b674535c643427a429a0
kashwa <kashwa80@gmail.com>
2020-06-21 15:21:16 +0200

    setup axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import '@/assets/styles/app.css'
+
+// Set Default-Base-URL for Axios.
+axios.defaults.baseURL = 'http://localhost:8080/backend-integration/public/api/'
 
 Vue.config.productionTip = false
 
