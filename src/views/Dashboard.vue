004cb45dc4c90d9bba4675127568aafd7cb98d3d
kashwa <kashwa80@gmail.com>
2020-07-11 16:10:04 +0200

    commenting table

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="home">
     
-    Dashboard
+    <!-- Reference button for create article modal -->
+
+    <!-- Listing of all articles -->
+    <!-- <table class="table-auto">
+      <thead>
+        <tr>
+          <th class="px-4 py-2">Title</th>
+          <th class="px-4 py-2">Author</th>
+          <th class="px-4 py-2">Views</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border px-4 py-2">Intro to CSS</td>
+          <td class="border px-4 py-2">Adam</td>
+          <td class="border px-4 py-2">858</td>
+        </tr>
+        <tr>
+          <td class="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+          <td class="border px-4 py-2">Adam</td>
+          <td class="border px-4 py-2">112</td>
+        </tr>
+        <tr>
+          <td class="border px-4 py-2">Intro to JavaScript</td>
+          <td class="border px-4 py-2">Chris</td>
+          <td class="border px-4 py-2">1,280</td>
+        </tr>
+      </tbody>
+    </table> -->
 
   </div>
 </template>
